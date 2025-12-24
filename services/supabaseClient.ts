074e1cdaed4ac8fd0d0673ba8dc018048616cd54
yoha <sushiyoha@proton.me>
2025-12-24 17:19:16 +0800

    deploy again

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// 请确保你的 .env.local 文件里有这两个变量
+// 请确保你的 .env.local 文件里有这两个变量><触发一下重新部署
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
